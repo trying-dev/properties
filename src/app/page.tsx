b09dc86e5be61e63972783c4738fab8a4baf5de8
revione <revione@outlook.com>
2025-12-09 20:21:14 +0100

    cache call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import Header from '+/components/Header'
 import Footer from '+/components/Footer'
 import { getAvailableUnits } from '+/actions/nuevo-proceso'
 import HomeClient from './HomeClient'
+import { unstable_cache as cache } from 'next/cache'
+
+const availableUnitsTag = 'available-units'
+const getCachedUnits = cache(() => getAvailableUnits({}), [availableUnitsTag], {
+  tags: [availableUnitsTag],
+  revalidate: 300, // refresca al siguiente request luego de 5 minutos o antes si se invalida la tag
+})
 
 export default async function HomePage() {
-  const units = await getAvailableUnits({})
+  const units = await getCachedUnits()
   return (
     <div className="min-h-screen bg-white">
       <Header />
